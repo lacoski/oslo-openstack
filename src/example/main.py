9a007b7509a41d0ce9402b848df80c2d4f525273
lacoski <123xuzu@gmail.com>
2018-12-08 16:49:49 +0700

    update test

--- src/example/main.py
+++ src/example/main.py
@@ -4,7 +4,8 @@ import oslo.messaging as om
 
 from oslo_messaging.transport import TransportURL
 
-rabbit://openstack:Welcome123@10.10.10.175
+
+rabbit://openstack:Welcome123@10.10.10.175//nova
 
 
 cfg.CONF.set_override('rabbit_host', '10.10.10.175')
@@ -20,3 +21,39 @@ pprint(res)
 
 
 http://fosshelp.blogspot.com/2015/02/openstack-oslo-messaging-rpc-api.html
+
+
+transport = messaging.get_rpc_transport(cfg.CONF)
+target = messaging.Target(topic='test', version='2.0')
+client = messaging.RPCClient(transport, target)
+client.call(ctxt, 'test', arg=arg)
+
+conf=cfg.CONF
+
+
+transport = oslo_messaging.get_rpc_transport(self.conf, url='fake:')
+client = oslo_messaging.RPCClient(transport, target)
+
+url, results, hide_password
+
+
+from oslo_config import cfg
+from oslo_messaging import TransportURL
+import oslo_messaging
+
+import oslo_messaging.transport
+oslo_messaging.transport.TransportURL(cfg.CONF, '10.10.10.175')
+trl = oslo_messaging.transport.TransportURL(cfg.CONF, '10.10.10.175')
+trl = oslo_messaging.transport.TransportURL(cfg.CONF, transport='rabbit://openstack:Welcome123@10.10.10.175//nova')
+
+class TestClient(object):
+
+    def __init__(self, transport):
+        target = messaging.Target(topic='test', version='2.0')
+        self._client = messaging.RPCClient(transport, target)
+
+    def test(self, ctxt, arg):
+        return self._client.call(ctxt, 'test', arg=arg)
+
+
+https://docs.openstack.org/oslo.messaging/latest/reference/target.html#target-versions
